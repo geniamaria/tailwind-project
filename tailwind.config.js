894132dc6040d46b7d1a06223f1bcc68fd0d317a
geniamaria <mariageniaabilio@gmail.com>
2023-04-14 13:12:33 +0200

    aplicando responsividade na nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html}"],
+  content: ["./*.html"],
   theme: {
+    screens: {
+      mobile: "320px",
+      tablet: "481px",
+      laptop: "1024px",
+    },
     extend: {},
   },
   plugins: [],
